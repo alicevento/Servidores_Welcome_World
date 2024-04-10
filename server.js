53f0fb679d1cca18b52a041725131172ad3d5035
Alicia Vento <aliciavento@gmail.com>
2024-04-10 19:43:37 -0400

    Commit 2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // importaciones, instanciar express y generar servidor
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
+const morgan = require('morgan');
 
 const app = express()
 const PORT = process.env.PORT || 3000;
@@ -10,25 +10,85 @@ app.listen(PORT, ()=> {
     console.log("Servidor Express en el puerto " + PORT);
 })
 
-// Middleware para parsear el cuerpo de la solicitud como JSON
-app.use(express.json());
-// Middleware para parsear los datos de los formularios
-app.use(express.urlencoded({ extended: true }));
+// Paso 1
+app.get("/crear", async (req, res) => {
+    // Paso 2
+    const { archivo, contenido } = req.query
+    fs.writeFile(archivo, contenido, () => {
+        res.status(200).send('Archivo creado con éxito!')
+    })
+})
 
+// Paso 4
+app.get("/leer", (req, res) => {
+    // Paso 5
+    const {archivo } = req.query
+    //console.log("Valor de parametro nombre: ", nombre)
+    //console.log("Tipo de dato parametro nombre: ", typeof nombre)
+    
+    // Paso 6
+    fs.readFile(archivo, (err, data) => {
+
+        let mensaje=""
+        if (err) {
+
+            (nombre)
+              ? mensaje = nombre + " No existe, "
+              : mensaje = "Falta archivo, "
+
+          return res.status(500).send(mensaje+err)
+        }
+
+        //console.log(data.toString())
+        //res.send(nombre + " ha sido leido con exito")
+        //res.send(data)
+        res.sendFile(__dirname + "/" + nombre)
+    })
+})
 
 
 // Paso 1
-app.get("/usuarios", (req, res) => {
-    // Paso 2 extraer con destructuring parametro de querystrings
-    const{ rut } = req.query
-    // Paso 3 buscar el rut de la ruta en el arreglos
-    const usuarioEncontrado = users.find(u => u.rut == Number(rut))
+app.get("/renombrar", (req, res) => {
 
-    // Paso 4 tomar accion si encontrado el rut o no
-    if(usuarioEncontrado) {
-        const { nombre, apellido } = usuarioEncontrado
-        res.send(`¡Usuario encontrado! Nombre: ${nombre} - Apellido: ${apellido}`)
-    } else{
-        res.send(`No se encontró ningún usuario con el rut ${rut}`)
-    }
+    // Paso 2
+    const{ nombre, nuevo } = req.query
+    //console.log("Valor de parametro nombre: ", nombre)
+    //console.log("Valor de parametro nombre: ", nuevo)
+    
+
+    // Paso 3
+    fs.rename(nombre, nuevo, (err) => {
+        let mensaje=""
+        if (err) {
+
+            (nombre)
+              ? mensaje = nombre + " No existe, "
+              : mensaje = "Falta archivo, "
+    
+          return res.status(500).send(mensaje+err)
+        }
+        res.status(200).send(`Archivo ${nombre} fue renombrado como ${nuevo}`)
+    })
 })
+
+// Paso 4
+app.get("/eliminar", (req, res) => {
+
+    // Paso 5
+    const{ nombre } = req.query
+
+    // Paso 6
+    fs.unlink(nombre, (err) => {
+        let mensaje=""
+        if (err) {
+
+            (nombre)
+              ? mensaje = nombre + " No existe, "
+              : mensaje = "Falta archivo, "
+    
+          return res.status(500).send(mensaje+err)
+        }
+        res.status(200).send(`Archivo ${nombre} eliminado con éxito`);
+    })
+})
+
