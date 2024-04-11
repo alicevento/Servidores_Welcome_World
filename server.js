538073c6c6d3f482e2672aed7e92b3233d3f8e84
MacarenaQuijadaG <mcarena.quijada@outlook.com>
2024-04-10 20:05:25 -0400

    Se suben cambios en e codigo espesificamente en el server con los CRUD

--- server.js
+++ server.js
@@ -1,34 +1,52 @@
-// importaciones, instanciar express y generar servidor
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+   
+   const express = require('express')
+   const app = express()
+   const fs = require('fs').promises
+   app.listen(3000, console.log("SERVER ON"))
+   // Vamos a utilizar levantar el index.html desde el servidor cuando se visite la ruta raíz
+   app.get("/", (req, res) => {
+       res.sendFile(__dirname + "/index.html")
+   })
 
-const app = express()
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, ()=> {
-    console.log("Servidor Express en el puerto " + PORT);
+   // Ruta crear
+app.get("/crear", async (req, res) => {
+    const { archivo, contenido } = req.query
+    try {
+        await fs.writeFile(archivo, contenido)
+        res.send("Archivo creado con éxito!")
+    } catch (error) {
+        res.status(500).send("Algo salió mal...")
+    }
 })
-
-// Middleware para parsear el cuerpo de la solicitud como JSON
-app.use(express.json());
-// Middleware para parsear los datos de los formularios
-app.use(express.urlencoded({ extended: true }));
-
-
-
-// Paso 1
-app.get("/usuarios", (req, res) => {
-    // Paso 2 extraer con destructuring parametro de querystrings
-    const{ rut } = req.query
-    // Paso 3 buscar el rut de la ruta en el arreglos
-    const usuarioEncontrado = users.find(u => u.rut == Number(rut))
-
-    // Paso 4 tomar accion si encontrado el rut o no
-    if(usuarioEncontrado) {
-        const { nombre, apellido } = usuarioEncontrado
-        res.send(`¡Usuario encontrado! Nombre: ${nombre} - Apellido: ${apellido}`)
-    } else{
-        res.send(`No se encontró ningún usuario con el rut ${rut}`)
+// Ruta leer
+app.get("/leer", async (req, res) => {
+    const { archivo } = req.query
+    try {
+        const data = await fs.readFile(archivo)
+        //res.send(data)
+        res.sendFile(__dirname + "/" + archivo)
+    }
+    catch (error) {
+        res.status(500).send("Algo salió mal...")
+    }
+})
+// Ruta renombrar
+app.get("/renombrar", async (req, res) => {
+    const {archivo, nuevoNombre} = req.query
+    try {
+        await fs.rename(archivo, nuevoNombre)
+        res.send(`Archivo ${archivo} renombrado por ${nuevoNombre}`)
+    } catch (error) {
+        res.status(500).send("Algo salió mal...")
+    }
+})
+// Ruta eliminar       
+app.get("/eliminar", async (req, res) => {
+    const { archivo } = req.query
+    try {
+        await fs.unlink(archivo)
+        res.send(`Archivo ${archivo} eliminado con éxito`)
+    } catch (error) {
+        res.status(500).send("Algo salió mal...")
     }
 })
